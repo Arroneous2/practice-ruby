83aef32aa2219a21c1c7b58b7aed5a89f01f2455
Arroneous2 <thumpinbassbeat@gmail.com>
2024-05-07 15:02:30 -0500

    enough_stock method

--- hash_for_store.rb
+++ hash_for_store.rb
@@ -7,27 +7,35 @@
 # p "#{item2[:name]} is the hot new toy which is #{item2[:color]} and #{item2[:size]} at the low low price of $#{item2[:price]}"
 
 class Item
-  attr_reader :name, :color, :size, :price
+  attr_reader :name, :color, :size, :price, :stock
   attr_writer :price
 
-  def initialize(input_name, input_color, input_size, input_price)
+  def initialize(input_name, input_color, input_size, input_price, input_stock)
     @name = input_name
     @color = input_color
     @size = input_size
     @price = input_price
+    @stock = input_stock
   end
 
   def sales_pitch()
     p "#{name} is the hot new toy which is #{color} and #{size} at the low low price of #{price}"
   end
 
-  def fire_sale()
-    @price = @price * 0.5
+  def fire_sale(percent_off)
+    @price = @price - @price * percent_off/100
   end
 
+  def enough_stock(threshold_stock)
+    if threshold_stock > @stock
+      p "We need to restock"
+    else
+      p "We're good to go"
+    end
+  end
 end
 
-item1 = Item.new("Hot Wheels", "yellow", "small", 10)
+item1 = Item.new("Hot Wheels", "yellow", "small", 10, 100)
 
 p item1
 item1.sales_pitch
@@ -35,5 +43,7 @@ p item1.price
 item1.price = 15
 p item1.price
 item1.sales_pitch
-item1.fire_sale
+item1.fire_sale(20)
 p item1.price
+item1.enough_stock(100)
+item1.enough_stock(101)
