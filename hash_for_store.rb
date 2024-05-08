e6ece62547175374777fd85df3cda809652ca480
Arroneous2 <thumpinbassbeat@gmail.com>
2024-05-08 12:44:24 -0500

    Employee and Manager class

--- hash_for_store.rb
+++ hash_for_store.rb
@@ -36,22 +36,51 @@ class Item
 end
 
 class Store
-  attr_reader :item, :current_shift_employees, :items_arry
-  attr_writer :item, :current_shift_employees,  
+  attr_reader :item, :current_shift_employee, :items_arry
+  attr_writer :item, :current_shift_employee,  
 
   def initialize(input)
     @items_arry = []
     @item = input[:item]
-    @current_shift_employees = input[:current_shift_employees] 
+    @current_shift_employee = input[:current_shift_employee]
+    @current_shift_employees = [] 
   end
 end
 
 
+class Employee
+  attr_reader :first_name, :last_name, :salary, :employed
+  attr_writer :first_name, :last_name, :salary, :employed
+
+  def initialize(input)
+    @first_name = input[:first_name]
+    @last_name = input[:last_name]
+    @salary = input[:salary]
+    @employed = input[:employed]
+  end
+
+  def print_info()
+    p "#{@first_name} #{@last_name} makes $#{@salary} per year."
+  end
+
+end
+
+class Manager < Employee
+
+  def send_report()
+    p "Blah blah blah"
+  end
+end
 
 item1 = Item.new({name: "Hot Wheels", color: "yellow", size: "small", price: 10, stock: 100})
-current_shift_employees1 = []
+current_shift_employee1 = []
 
-store1 = Store.new({item: item1, current_shift_employees: current_shift_employees1})
-p store1
+store1 = Store.new({item: item1, current_shift_employee: current_shift_employee1})
+
+employee1 = Employee.new(first_name: "Aaron", last_name: "Riggs", salary: 90000, employed: true)
+
+employee1.print_info
+
+
 
 
